ff91b5b362fbacabe25518513e0577d2bd8e4960
정지민 <rindalica@gmail.com>
2022-01-25 19:40:33 +0900

    Add: 카테고리 메뉴 버튼 클릭시 nav 전체 및 버튼 글씨 색 변경

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,20 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Nav.scss';
+
+// import { FiSearch } from 'react-icons/fi';
 
 function Nav() {
-  return (
-    <div>
-      <ul>
-        <li>
-          <button>{name}</button>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <button>로그인</button>
-        </li>
-      </ul>
-    </div>
-  );
   const categoryData = [
     { name: '스킨케어' },
     { name: '바디&헨드' },
@@ -26,6 +15,46 @@ function Nav() {
     { name: '읽기' },
     { name: '스토어' },
   ];
+
+  const [isClick, setIsClick] = useState(false);
+  const [navClassName, setNavClassName] = useState('nav');
+  const handleClick = () => {
+    setIsClick(!isClick);
+    isClick ? setNavClassName('nav') : setNavClassName('navClick');
+  };
+
+  return (
+    <div className={navClassName}>
+      <nav className="primaryMenu">
+        <ul className="mainCategories">
+          {categoryData.map(data => {
+            return (
+              <li className="mainCategory">
+                <button onClick={handleClick} className="mainCategoryBtn">
+                  {data.name}
+                </button>
+              </li>
+            );
+          })}
+          <li className="mainCategory">
+            <button className="mainCategoryBtn">
+              검색{/* <FiSearch /> */}
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <nav className="secondaryMenu">
+        <ul className="secondaryMenuList">
+          <li className="loginMenu">
+            <button className="loginBtn">로그인</button>
+          </li>
+          <li className="cartMenu">
+            <button className="cartBtn">카트</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Nav;
