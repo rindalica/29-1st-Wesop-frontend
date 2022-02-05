2bd07f4fb6e71dd006f65a8f9fc4473eb463cade
memoyoon <hazelmo0109@gmail.com>
2022-02-05 20:08:16 +0900

    after carousel, before css

--- src/pages/ProductsList/SkinProductsListRow/SkinProductsListRow.js
+++ src/pages/ProductsList/SkinProductsListRow/SkinProductsListRow.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SkinProduct from '../SkinProduct/SkinProduct';
 import './SkinProductsListRow.scss';
 
-function SkinProductsListRow({ name, description, productsList }) {
+function SkinProductsListRow({ className, name, description, productsList }) {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const onClick = event => {
+    const isPrev = event.target.className.includes('prev');
+    const isNext = event.target.className.includes('next');
+    if (isPrev) {
+      setSlideIndex(curr => --curr);
+    }
+    if (isNext) {
+      setSlideIndex(curr => ++curr);
+    }
+  };
+
+  const firstSlide =
+    slideIndex === 0
+      ? {
+          style: { transform: 'translateX(-80px)' },
+        }
+      : null;
+
+  const lastSlide =
+    slideIndex === productsList.length - 3
+      ? {
+          style: { transform: 'translateX(80px)' },
+        }
+      : null;
   return (
     <div className="skinProductsListRow">
       <div className="skinSubcatIntro">
         <h1>{name}</h1>
         <p>{description}</p>
       </div>
-      {productsList &&
+      {/* {productsList &&
         productsList.map(product => {
           return (
             <SkinProduct
@@ -18,7 +44,47 @@ function SkinProductsListRow({ name, description, productsList }) {
               detail={product.product_detail}
             />
           );
-        })}
+        })} */}
+      <div className={`Carousel ${className}`}>
+        <button
+          className="navigation prev"
+          onClick={onClick}
+          style={firstSlide && firstSlide.style}
+          disabled={firstSlide}
+        >
+          <i className="fas fa-chevron-left prev" />
+        </button>
+        <div
+          className="itemWrapper"
+          style={{ transform: `translateX(calc(-100% / 3 * ${slideIndex}))` }}
+        >
+          {productsList &&
+            productsList.map(product => (
+              <SkinProduct
+                key={product.product_name}
+                name={product.product_name}
+                detail={product.product_detail}
+              />
+            ))}
+        </div>
+        <button
+          className="navigation next"
+          onClick={onClick}
+          style={lastSlide && lastSlide.style}
+          disabled={lastSlide}
+        >
+          <i className="fas fa-chevron-right next" />
+        </button>
+        <div className="pagination">
+          <div
+            className="current"
+            style={{
+              width: `calc(100% / ${productsList.length - 2})`,
+              transform: `translateX(calc(100% * ${slideIndex}))`,
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
