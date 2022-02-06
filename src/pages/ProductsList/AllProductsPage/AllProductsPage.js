e4a912ff99236c92a2a72ca81090ca591d1ad55b
memoyoon <hazelmo0109@gmail.com>
2022-02-06 16:28:30 +0900

    Router.js 링크주소 변경

---
+++ src/pages/ProductsList/AllProductsPage/AllProductsPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AllProductsPage() {
+  return <div />;
+}
+
+export default AllProductsPage;
--- src/pages/ProductsList/ProductsList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ProductsList = () => {
-  return <div />;
-};
-
-export default ProductsList;
