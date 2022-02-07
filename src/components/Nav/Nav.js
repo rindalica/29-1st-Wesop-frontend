c7c52ed206b87b629979de9502df4be0a253853e
정지민 <rindalica@gmail.com>
2022-02-07 14:32:34 +0900

    border 삭제!

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -18,21 +18,7 @@ function Nav() {
         setCategoryLists(data);
       });
   }, []);
-  //console.log(categoryLists);
-  const [border, setBorder] = useState('mainCategory');
-  // const [isClick, setIsClick] = useState(false);
-  // const handle = a => {
-  //   setIsClick(!isClick);
-  //   if (isClick === true) {
-  //     if (a === 'mainCategorySkin') {
-  //       return 'mainCategorySkin';
-  //     } else if (a === 'mainCategoryBody') {
-  //       return 'mainCategoryBody';
-  //     }
-  //   } else {
-  //     return 'mainCategory';
-  //   }
-  // };
+
   return (
     <div className="nav">
       <div className={navColor}>
@@ -41,14 +27,13 @@ function Nav() {
             {categoryLists.map(
               ({ id, categoryName, className, categoryImg, liClassName }) => {
                 return (
-                  <li key={id} className={border}>
+                  <li key={id} className="mainCategory">
                     <button
                       onClick={function handleModal() {
                         setModal('categoryModalBox' + className);
                         setNavColor('categoryNavModal');
                         setCategoryLists(categoryLists);
                         setCategoryImg(categoryImg);
-                        setBorder(liClassName);
                       }}
                       className="mainCategoryBtn"
                     >
