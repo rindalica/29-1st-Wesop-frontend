24e19742207ef99d79cc8821ed46d2c47bf6f2a7
정지민 <rindalica@gmail.com>
2022-02-10 13:16:40 +0900

    refactoring (통신 연결 필요)

--- src/components/Nav/Cart/CartModal.js
+++ src/components/Nav/Cart/CartModal.js
@@ -1,45 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './CartModal.scss';
 import CartProduct from './CartProduct';
+// import { BASE_URL } from '../../../config';
+
 function CartModal({ cartModal }) {
   const [cartData, setCartData] = useState([]);
 
-  //useEffect(() => {
-  //   fetch('/data/CartData.json')
-  //     .then(res => res.json())
-  //     .then(res => setCartData(res));
-  //   // .then(res => console.log(res));
-  // }, []);
-  //   fetch('http://10.58.6.120:8000/carts/1')
-  //     .then(res => res.json())
-  //     .then(res => console.log(res));
-  // }, []);
-
-  // }, []);
-  // const [totalPrice, setTotalPrice] = useState(0);
-  // const total = cartData
-  //   .map(item => item.price * item.selected)
-  //   .reduce((prev, curr) => prev + curr, 0);
-  // const sumPrice = () => {
-  //   setTotalPrice(1);
-  // };
-  const test = () => {
-    fetch('http://10.58.6.120:8000/carts/add', {
-      method: 'post',
-      headers: {
-        // 헤더 조작
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        message: {
-          product_options_id: 2,
-          user_id: 1,
-        },
-      }),
-    })
+  useEffect(() => {
+    fetch('http://localhost:3000/data/CartDatas.json')
       .then(res => res.json())
-      .then(res => console.log(res));
-  };
+      .then(data => {
+        setCartData(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className={cartModal}>
       <div className="cartProducts">
@@ -47,29 +22,19 @@ function CartModal({ cartModal }) {
           <div>카트</div>
           <div>사이즈</div>
           <div>수량</div>
-          <button className="closeModalBtn" onClick={test}>
-            X
-          </button>
+          <button className="closeModalBtn">X</button>
         </div>
-        <ul className="CartProductsList">
+        <ul>
           {cartData.map(product => {
             function onDelete() {
               setCartData(
-                cartData.filter(it => {
-                  if (it.id !== product.id) {
+                cartData.filter(select => {
+                  if (select.id !== product.id) {
                     return product.id;
                   }
                 })
               );
-              //               fetch(
-              //               //TODO
-              //               //path파라미터
-              // , {method : "DELETE"}
-              //               )
-              //               .then(res => res.json())
-              //               .then(res => console.log(res.result))
             }
-
             return (
               <CartProduct
                 key={product.id}
@@ -87,9 +52,9 @@ function CartModal({ cartModal }) {
         <span>전 제품 무료 배송 혜택을 즐겨보세요.</span>
         <div className="cartSummaryMoney">
           <h5> 소계 (세금 포함)</h5>
-          <span> ₩ </span>
+          <span> ₩ 0원</span>
         </div>
-        <button>결제하기</button>
+        <button className="payBtn">결제하기</button>
       </div>
     </div>
   );
--- src/components/Nav/Cart/CartProduct.js
+++ src/components/Nav/Cart/CartProduct.js
@@ -1,29 +1,18 @@
 import React, { useState } from 'react';
 import Option from './Option';
 
-function CartProduct({ key, productName, size, option, onClick, price }) {
+function CartProduct({ productName, size, option, onClick, price }) {
   const [selected, setSelected] = useState(1);
-  const handleValue = e => {
+
+  const selectValue = e => {
     setSelected(e.target.value);
   };
-  //TODO
-  // fetch('/data/CartData.json/1', {
-  //   method: 'post',
-  //   body: JSON.stringify({
-  //     name: 'qu',
-  //     Quantity: { selected },
-  //   }),
-  // })
-  //   .then(res => res.json())
-  //   .then(res => {
-  //     console.log(res);
-  //   });
 
   return (
-    <li className="cartProduct" key={key}>
+    <li className="cartProduct">
       <div>{productName}</div>
       <div>{size}</div>
-      <Option option={option} onChange={handleValue} onClick={onClick} />
+      <Option option={option} onChange={selectValue} onClick={onClick} />
       <div> ₩ {price * selected}</div>
     </li>
   );
